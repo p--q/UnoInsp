58661bcdcfe5097f4defdf3170bea997524aa928
pq <pq.blogspot@gmail.com>
2017-03-29 00:50:28 +0900

    HM

--- UnoInsp/src/job_ex.py
+++ UnoInsp/src/job_ex.py
@@ -1,35 +1,35 @@
 #!/opt/libreoffice5.2/program/python
 # -*- coding: utf-8 -*-
 import unohelper
- 
+
 # interfaces
 from com.sun.star.task import XJob
 from com.sun.star.lang import XServiceInfo
- 
+
 ImplName = "mytools.basicide.IATest"
 ServiceNames = ("com.sun.star.task.Job",)
- 
-class IATest(unohelper.Base, XJob, XServiceInfo):
-    
+
+# class IATest(unohelper.Base, XJob, XServiceInfo):
+class IATest(unohelper.Base, XServiceInfo):
     def __init__(self,ctx):
         self.ctx = ctx
         print("initialized")
-    
+
     # XJob
     def execute(self, args):
         print("job executed.")
-    
+
     # XServiceInfo
     def getImplementationName(self):
         return ImplName
-    
+
     def supportsService(self, name):
         return name in ServiceNames
-    
+
     def getSupportedServiceNames(self):
         return ServiceNames
- 
- 
+
+
 g_ImplementationHelper = unohelper.ImplementationHelper()
 g_ImplementationHelper.addImplementation(
     IATest, ImplName, ServiceNames)
